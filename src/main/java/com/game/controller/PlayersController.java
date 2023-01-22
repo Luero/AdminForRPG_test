521f204c2943297af90e2eba79b1fe4ca9f1e304
Ksu Er <kseniapodguzova@gmail.com>
2023-01-22 16:12:19 +0300

    Update get method getPlayers in the controller class, create support methods in the service class.

--- src/main/java/com/game/controller/PlayersController.java
+++ src/main/java/com/game/controller/PlayersController.java
@@ -1,11 +1,14 @@
 package com.game.controller;
 
+import com.game.entity.Profession;
+import com.game.entity.Race;
 import com.game.service.PlayersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import com.game.entity.Player;
 
@@ -22,9 +25,28 @@ public class PlayersController {
         this.playersService = playersService;
     }
 
-    @GetMapping("/players")
-    public @ResponseBody ResponseEntity<List<Player>> getPlayersList() {
-        return new ResponseEntity<List<Player>>(playersService.getPlayers(), HttpStatus.OK);
+    @RequestMapping(path = "/players", method = RequestMethod.GET)
+    public List<Player> getPlayersList(@RequestParam(value = "name", required = false) String name,
+                                                                     @RequestParam(value = "title", required = false) String title,
+                                                                     @RequestParam(value = "race", required = false) Race race,
+                                                                     @RequestParam(value = "profession", required = false) Profession profession,
+                                                                     @RequestParam(value = "after", required = false) Long after,
+                                                                     @RequestParam(value = "before", required = false) Long before,
+                                                                     @RequestParam(value = "banned", required = false) Boolean banned,
+                                                                     @RequestParam(value = "minExperience", required = false) Integer minExperience,
+                                                                     @RequestParam(value = "maxExperience", required = false) Integer maxExperience,
+                                                                     @RequestParam(value = "minLevel", required = false) Integer minLevel,
+                                                                     @RequestParam(value = "maxLevel", required = false) Integer maxLevel,
+                                                                     @RequestParam(value = "order", required = false) PlayerOrder order,
+                                                                     @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                                                     @RequestParam(value = "pageSizw", required = false) Integer pageSize) {
+
+        final List<Player> players = playersService.getPlayers(name, title, race, profession, after, before, banned, minExperience,
+                maxExperience, minLevel, maxLevel);
+
+        final List<Player> sortedPlayers = playersService.sortPlayers(players, order);
+
+        return playersService.getPages(sortedPlayers, pageNumber,pageSize);
     }
 
     @GetMapping("/players/{id}")
--- src/main/java/com/game/service/PlayersService.java
+++ src/main/java/com/game/service/PlayersService.java
@@ -1,14 +1,22 @@
 package com.game.service;
 
+import com.game.controller.PlayerOrder;
 import com.game.entity.Player;
+import com.game.entity.Profession;
+import com.game.entity.Race;
 import com.game.repository.PlayersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import static java.lang.Long.valueOf;
 
 @Service
 public class PlayersService {
@@ -20,12 +28,71 @@ public class PlayersService {
     }
 
     //Get players list
-    public List<Player> getPlayers() {
-        List<Player> allPlayers = playersRepository.findAll();
+    public List<Player> getPlayers(String name, String title, Race race, Profession profession, Long after, Long before,
+                                   Boolean banned, Integer minExperience, Integer maxExperience, Integer minLevel, Integer maxLevel)
+    {
+        final Date dateAfter = after == null ? null : new Date(after);
+        final Date dateBefore = before == null ? null : new Date(before);
+        final List<Player> allPlayers = new ArrayList<>();
 
-        //TODO
+        playersRepository.findAll().forEach((player) -> {
+            if(name != null && !player.getName().contains(name)) return;
+            if(title != null && !player.getTitle().contains(title)) return;
+            if(race != null && !player.getRace().equals(race)) return;;
+            if (profession != null && !player.getProfession().equals(profession)) return;
+            if(after != null && player.getBirthday().before(dateAfter)) return;
+            if(before != null && player.getBirthday().after(dateBefore)) return;
+            if(banned != null && !player.getBanned() != banned) return;
+            if(minExperience != null && player.getExperience() < minExperience) return;
+            if(maxExperience != null && player.getExperience() > maxExperience) return;
+            if(minLevel != null && player.getLevel() < minLevel) return;
+            if(maxLevel != null && player.getLevel() > maxLevel) return;
 
+            allPlayers.add(player);
+        });
         return allPlayers;
+    }
+
+    public List<Player> sortPlayers(List<Player> players, PlayerOrder playerOrder)
+    {
+        players.sort((player1, player2) -> {
+            switch (playerOrder) {
+                case NAME:
+                    return player1.getName().compareTo(player2.getName());
+                case LEVEL:
+                    return player1.getLevel().compareTo(player2.getLevel());
+                case BIRTHDAY:
+                    return player1.getBirthday().compareTo(player2.getBirthday());
+                case EXPERIENCE:
+                    return player1.getExperience().compareTo(player2.getExperience());
+                default:
+                    Long player1ID = valueOf(player1.getId());
+                    Long player2ID = valueOf(player2.getId());
+                    return player1ID.compareTo(player2ID);
+                }
+        });
+
+        return players;
+    }
+
+    public List<Player> getPages(List<Player> players, Integer pageNumber, Integer pageSize)
+    {
+        if (pageNumber == null)
+        {
+            pageNumber = 0;
+        }
+        if (pageSize == null)
+        {
+            pageSize = 3;
+        }
+        final int fromPage = pageNumber * pageSize;
+        int toPage = fromPage + pageSize;
+
+        if(toPage > players.size())
+        {
+            toPage = players.size();
+        }
+        return players.subList(fromPage, toPage);
     }
 
     //Get players count
